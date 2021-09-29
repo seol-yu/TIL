9e038eaaa43d9c5422885d97c966c3116e5cc411
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2021-09-30 08:27:28 +0900

    TS 기본 타입 - JS 간단 복습

---
+++ TypeScript/TS_OOP/01_기본_타입/1-types/1-1-basic.ts
@@ -0,0 +1,14 @@
+{
+    // JavaScript
+    // old: var 💩
+    var age = 5;
+    age = 1;
+
+    // let es6
+    let name = 'hello';
+    name = 'hi';
+
+    // const - 한번 선언하면 끝
+    const age = 5;
+    // age = 5;
+}
