19309d1165da7c2d752a9f3add167df33a4e84c4
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2021-10-03 18:19:44 +0900

    모듈 | export, require

---
+++ NodeJS/노드_백엔드/02_Node_Modules/4-module/counter.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function increase() {
+    count++;
+}
+
+function getCount() {
+    return count;
+}
+
+module.exports.getCount = getCount;
+// module.exports.increase = increase;
+console.log(module.exports === exports);  // true
+exports = {};
+console.log(module.exports === exports);  // false
+exports.increase = increase;
+console.log(module);
