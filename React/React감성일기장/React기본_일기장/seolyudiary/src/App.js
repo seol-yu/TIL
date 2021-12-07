a984bed9e315dd1af1d3219942393e0459478b3e
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2021-12-07 23:45:31 +0900

    React | 리스트 렌더링(조회)

--- React/React감성일기장/React기본_일기장/seolyudiary/src/App.js
+++ React/React감성일기장/React기본_일기장/seolyudiary/src/App.js
@@ -2,11 +2,35 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "이설유",
+    content: "내용1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "이설유2",
+    content: "내용2",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "이설유3",
+    content: "내용3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
-      <DiaryList />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
