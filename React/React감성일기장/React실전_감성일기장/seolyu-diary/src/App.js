a7388ad4f8696f83b370f675f75d1365a642d64f
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2021-12-17 23:00:00 +0900

    React | 프로젝트 기초 세팅

--- React/React감성일기장/React실전_감성일기장/seolyu-diary/src/App.js
+++ React/React감성일기장/React실전_감성일기장/seolyu-diary/src/App.js
@@ -1,23 +1,57 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RouteTest from "./components/RouteTest";
+
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTs
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
-      <div className="App">Seolyu</div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/edit" element={<Edit />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        {/* <Route path="/diary" element={<Diary />} /> */}
-      </Routes>
-      <RouteTest />
+      <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+          {/* <Route path="/diary" element={<Diary />} /> */}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
