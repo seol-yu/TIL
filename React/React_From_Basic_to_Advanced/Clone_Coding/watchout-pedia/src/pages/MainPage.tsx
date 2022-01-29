08fd832776623930265f947e9b66bfd6f763de59
Seolyu, Lee <leeseolyu@gmail.com>
2022-01-29 11:00:00 +0900

    [React] 클론코딩 Footer Design

--- React/React_From_Basic_to_Advanced/Clone_Coding/watchout-pedia/src/pages/MainPage.tsx
+++ React/React_From_Basic_to_Advanced/Clone_Coding/watchout-pedia/src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 const MainPage: React.FC = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
