424c99c72a24a5895c8f62bdd126a71c95cb9b63
Seolyu, Lee <leeseolyu@gmail.com>
2022-02-08 11:00:00 +0900

    리액트 네이티브 공부

--- React_Native/React_Native_Ko/ES6_React/react_01/App.js
+++ React_Native/React_Native_Ko/ES6_React/react_01/App.js
@@ -7,12 +7,49 @@
  */
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import PropsChild from './propsChild';
 
 class App extends Component {
+    state = {
+        sampleText: 'Hello World',
+        sampleBoolean: true,
+        sampleNum: 1
+    }
+
+    inputText = () => (
+    this.state.sampleBoolean ?
+    <Text>SampleBoolean is true</Text> :
+    <Text>SampleBoolean is false</Text>
+    )
+
+    changeState = () => {
+        if(!this.state.sampleBoolean) {
+            this.setState({
+                sampleText: 'Text Changed!!!',
+                sampleBoolean: true
+            })
+        } else {
+            this.setState({
+                sampleText: 'Hello World!!!',
+                sampleBoolean: false
+            })
+        }
+    }
+
+    onAdd = () => {
+        this.setState(prevState => {
+            return {
+                sampleNum: prevState.sampleNum + 1
+            }
+            //sampleNum: sampleNum + 1
+            //sampleNum: 100
+        })
+    }
+
     render() {
         return (
             <View style={styles.background}>
-            	<Text> Hello World </Text>
+                <PropsChild sText={this.state.sampleText} cState={this.changeState} />
             </View>
         )
     }
