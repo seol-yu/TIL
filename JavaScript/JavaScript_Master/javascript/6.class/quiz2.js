528b53b5ed3fbf5e081e1c7b9cea40deff408883
Seolyu, Lee <leeseolyu@gmail.com>
2022-04-06 11:00:00 +0900

    JavaScript | 클래스

--- JavaScript/JavaScript_Master/javascript/6.class/quiz2.js
+++ JavaScript/JavaScript_Master/javascript/6.class/quiz2.js
@@ -3,3 +3,34 @@
 // 매달 직원들의 정보를 이용해서 한달 월급을 계산할 수 있다
 // 정직원은 시간당 10000원
 // 파트타임 직원은 시간당 8000원
+
+class Employee {
+  constructor(name, department, hoursPerMonth, payRate) {
+    this.name = name;
+    this.department = department;
+    this.hoursPerMonth = hoursPerMonth;
+    this.payRate = payRate;
+  }
+  calculatePay() {
+    return this.hoursPerMonth * this.payRate;
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  static PAY_RATE = 10000;
+  constructor(name, department, hoursPerMonth) {
+    super(name, department, hoursPerMonth, FullTimeEmployee.PAY_RATE);
+  }
+}
+
+class PartTimeEmployee extends Employee {
+  static PAY_RATE = 8000;
+  constructor(name, department, hoursPerMonth) {
+    super(name, department, hoursPerMonth, PartTimeEmployee.PAY_RATE);
+  }
+}
+
+const seolyu = new FullTimeEmployee('설유', 's/w', 30);
+const yeasul = new PartTimeEmployee('예슬', 's/w', 20);
+console.log(seolyu.calculatePay());
+console.log(yeasul.calculatePay());
